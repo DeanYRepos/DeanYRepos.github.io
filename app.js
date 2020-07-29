f6e212bd1e18ace631080236ad43de46c9429927
Dean Y <54600923+DeanYRepos@users.noreply.github.com>
2020-07-29 14:00:12 -0500

    upload to repo

---
+++ app.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const {projects} = require('./data.json');
+const port = process.env.PORT || 3000;
+const app = express();
+
+app.set('view engine', 'pug');
+app.use(express.json());
+app.use('/static', express.static('public'));
+
+
+app.get('/',(req, res, next) => {
+    res.render('index', {projects});
+    next();
+}) 
+app.get('/about',(req,res,next) => {
+    res.render('about');
+    next();
+})
+app.get('/projects/:id',(req,res,next) => {
+    const projectId = req.params.id;
+    
+    const project = projects.find( ({ id }) => id === +projectId );
+    if (project) {
+    res.render('project', { project });
+
+    } else {
+        
+        res.sendStatus(404);
+    }
+    next();
+});  
+
+app.use((req, res, next) => {
+
+    const err = new Error('Oh no! Page not found!');
+    err.status = 404;
+    next(err);
+});
+
+app.use((err, req, res, next) => {
+    res.locals.error = err;
+    res.status(err.status);
+    res.render('error');
+    console.log("Oop, page not found!");
+  
+}); 
+
+
+
+
+app.listen(port, () => {
+
+    console.log("Listening on port 3000!")
+});
